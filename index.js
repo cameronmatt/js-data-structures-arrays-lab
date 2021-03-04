37b3f770ca8c6f3c461d7ac2fe8247027946a633
Matthew <cameron.matt@outlook.com>
2021-03-04 17:51:17 +1100

    Arrays done

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(){
+    drivers.push('Ralph');
+}
+
+function destructivelyPrependDriver(){
+    drivers.unshift('Bob');
+}
+
+function destructivelyRemoveLastDriver() {
+    drivers.pop()
+}
+
+function destructivelyRemoveFirstDriver() {
+    drivers.shift()
+}
+
+function appendDriver() {
+    const drivers = [...drivers, 'Broom'];
+}
+
+function appendDriver() {
+    return [...drivers, 'Broom'];
+}
+
+function prependDriver() {
+    return ['Arnold', ...drivers];
+}
+
+function removeLastDriver() {
+    return drivers.slice(0, 2)
+}
+
+function removeFirstDriver() {
+    return drivers.slice(1)
+}
